4f51ce90140416e61a339a3ffd1197aff335fb2f
Andrew Acin <andrew_acin@hotmail.com>
2024-08-26 19:51:39 -0700

    installing and configuring Flask-SQLAlchemy

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,8 +1,17 @@
+# config
 from flask import Flask 
+from flask_migrate import Migrate
 
-
+# factory
 def create_app(): 
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:HEAC63@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from . import models
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
 
 
     @app.route('/')
@@ -19,3 +28,4 @@ def create_app():
 
 
     return app
+
